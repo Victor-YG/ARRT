ec09194b25687bad449fda4e423fe23e4aba6b6d
Victor-YG <yizhougao8@gmail.com>
2023-03-26 21:46:08 -0400

    Implemented the find_neighbors function.

--- src/common.py
+++ src/common.py
@@ -46,7 +46,29 @@ class Node_Collection:
         self.node_map[key].append(node)
 
 
+    def get_search_keys(self, query_key):
+        '''get all keys to regions to be searched'''
+
+        key_x = query_key[0]
+        key_y = query_key[1]
+
+        keys = [
+            (key_x + 0, key_y + 0),
+            (key_x - 1, key_y + 0),
+            (key_x + 1, key_y + 0),
+            (key_x + 0, key_y + 1),
+            (key_x + 0, key_y - 1),
+            (key_x - 1, key_y + 1),
+            (key_x - 1, key_y - 1),
+            (key_x + 1, key_y + 1),
+            (key_x + 1, key_y - 1)
+        ]
+
+        return keys
+
     def find_closest_in_region(self, env, position, key, min_dist):
+        '''find closest reacheable node in a region given its key'''
+
         closest_node = None
         local_min_dist = min_dist
 
@@ -64,20 +86,12 @@ class Node_Collection:
 
 
     def find_cloest_node(self, env, position):
+        '''find closest reacheable node to given position'''
+
         key_x = int(np.floor(position[0]))
         key_y = int(np.floor(position[1]))
 
-        keys = [
-            (key_x + 0, key_y + 0),
-            (key_x - 1, key_y + 0),
-            (key_x + 1, key_y + 0),
-            (key_x + 0, key_y + 1),
-            (key_x + 0, key_y - 1),
-            (key_x - 1, key_y + 1),
-            (key_x - 1, key_y - 1),
-            (key_x + 1, key_y + 1),
-            (key_x + 1, key_y - 1)
-        ]
+        keys = self.get_search_keys((key_x, key_y))
 
         min_dist = 1000
         closest_node = None
@@ -91,5 +105,29 @@ class Node_Collection:
         return closest_node, min_dist
 
 
-    def find_neigbhors(self, env, position, distance=1.0):
-        pass
+    def find_neighbors_in_region(self, env, position, key, dist_thres):
+        '''find all reachable node that is within distance threshold to given position'''
+        neighbors = []
+
+        for node in self.node_map[key]:
+            dist = np.sqrt(np.dot(node.position - position, node.position - position))
+            if dist < dist_thres:
+                if env.is_reachable(node.position, position):
+                    neighbors.append(node)
+
+        return neighbors
+
+
+    def find_neigbhors(self, env, position, dist_thres=1.0):
+        '''find all reachable node that is within distance threshold to given position'''
+
+        key_x = int(np.floor(position[0]))
+        key_y = int(np.floor(position[1]))
+
+        keys = self.get_search_keys((key_x, key_y))
+        neigbhors = []
+
+        for key in keys:
+            neigbhors.extend(self.find_neighbors_in_region(env, position, key, dist_thres))
+
+        return neigbhors
